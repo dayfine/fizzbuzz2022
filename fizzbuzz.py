4141d43397b7d6579d7721883f71ff05ba28fd71
Di Fan <26423504+dayfine@users.noreply.github.com>
2022-01-30 17:10:45 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+from typing import Callable, List
+
+ProduceFn = Callable[[int], str]
+
+def combine_strs(inputs: List[str]) -> str:
+	return ''.join(inputs)
+
+def divisible_by(k: int, n: int) -> bool:
+	return n % k == 0
+
+def produce_if_divisible_by_fn(output: str, k: int) -> ProduceFn:
+	return lambda n : output if divisible_by(k, n) else ''
+
+def combine_produce_fns(fns: List[ProduceFn]) -> ProduceFn:
+	return lambda n : combine_strs([fn(n) for fn in fns]) or f'{n}'
+
+fizzbuzz: ProduceFn = combine_produce_fns([
+		produce_if_divisible_by_fn('Fizz', 3),
+		produce_if_divisible_by_fn('Buzz', 5),
+	])
